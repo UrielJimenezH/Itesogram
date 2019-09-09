f85ca2dcbcd5d6b5dac7859a461613ee351adc40
Uriel <urieljh11@gmail.com>
2019-09-08 20:45:15 -0500

    Creacion del proyecto y conección con Parse server

---
+++ app/src/main/java/com/uriel/itesogram/App.kt
@@ -0,0 +1,18 @@
+package com.uriel.itesogram
+
+import com.parse.Parse
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId("v1kx8QckHwFoOhVF6JqOIkY77Y3ancPhksQnt4k6")
+                // if defined
+                .clientKey("fVER38fcRHHMHtrS10xq8BnTAsQdrDKvoXSR3maP")
+                .server("https://parseapi.back4app.com/")
+                .build()
+        )
+    }
+}
