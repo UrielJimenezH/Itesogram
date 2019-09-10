14f304a7970b6d1aae24d3d191abb75a592f606a
Uriel <urieljh11@gmail.com>
2019-09-10 15:33:48 -0500

    Diseño de la vista Register terminado

--- app/src/main/java/com/uriel/itesogram/activities/Login.kt
+++ app/src/main/java/com/uriel/itesogram/activities/Login.kt
@@ -3,7 +3,9 @@ package com.uriel.itesogram.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.uriel.itesogram.R
+import com.uriel.itesogram.Register
 import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 
@@ -11,15 +13,23 @@ class Login : AppCompatActivity() {
 
     private lateinit var btnLogin: Button
 
+
+    private lateinit var tvRegister: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
 
         btnLogin = find(R.id.activity_login_btn_login)
+        tvRegister = find(R.id.activity_login_tv_register)
 
         btnLogin.setOnClickListener {
             startActivity<SaveInParse>()
+        }
+
+        tvRegister.setOnClickListener {
+            startActivity<Register>()
         }
     }
 }
